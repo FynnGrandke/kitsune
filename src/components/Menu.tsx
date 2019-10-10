660ba888b2173b0cd128c9e290c0a6ed8c981abc
Fynn Grandke <fynn.grandke@web.de>
2019-10-10 18:07:07 +0900

    Added Menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Menu = () => {
+  return (
+    <div className="absolute flex flex-row content-between z-10 menuButton">
+      <button className="text-4xl overflow-hidden m-4">❤️</button>
+      <button className="text-5xl overflow-hidden m-4">💌</button>
+    </div>
+  );
+};
