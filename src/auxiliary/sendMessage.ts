22e3a20a1dcf33573d990969bdd926fd232a4ef2
Fynn Grandke <fynn.grandke@web.de>
2019-10-14 23:33:15 +0900

    Removed console.log

--- src/auxiliary/sendMessage.ts
+++ src/auxiliary/sendMessage.ts
@@ -4,11 +4,7 @@ export const sendMessage = (data) => {
   const request = new XMLHttpRequest();
   const dataAsString = JSON.stringify(data);
   
-  console.log('Data', dataAsString);
-
   request.open('POST', '/entries', true);
   request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   request.send(dataAsString);
-
-  console.log(`${dataAsString} sent`);
 };
